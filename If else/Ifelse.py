b6fee459b31dcc0cc00245c7e415b088d07b5211
bacdat009 <da.mobile992@gmail.com>
2015-08-26 21:18:37 +0700

    If else

---
+++ If else/Ifelse.py
@@ -0,0 +1,9 @@
+if score>=90:
+    print(A)
+    print(B)
+elif score>=70:
+    print(C)
+elif score>=60:
+    print(D)
+    else:
+    print(F)
--- democlass.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Admin'
-name = 'A'
-name2 = 'A' + 'B'
-if name == name2:
-    print('both are equal')
-else:
-    print('both not equal')
