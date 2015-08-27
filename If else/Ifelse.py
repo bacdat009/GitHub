2c59fc5458a4a7d7bce5a715a1d912f447ff0862
bacdat009 <da.mobile992@gmail.com>
2015-08-27 12:05:33 +0700

    is ==

--- If else/Ifelse.py
+++ If else/Ifelse.py
@@ -1,9 +1,28 @@
-if score>=90:
-    print(A)
-    print(B)
-elif score>=70:
-    print(C)
-elif score>=60:
-    print(D)
-    else:
-    print(F)
+name = 'abc'
+#name2 = 'a' + 'b' +'c'
+name2 = 'join'
+print(name)
+if name==name2:
+    print('both are equal')
+else:
+    print('both not equal')
+if name is name2:
+    print('They are same')
+else:
+    print('They not same')
+    print(id(name), id(name2))
+    if name is not name2:
+        print('they are two value')
+        x = 2
+        print(x)
+        y = 3/4
+        print(y)
+        if x==y:
+            print('both')
+        else:
+            print('not same')
+            if x is y:
+                print('This relly true')
+            else:
+                print('may be true')
+                print(id(x),id(y))
